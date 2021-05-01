77feeeb1497f794a58001ddcdd345c2f7e6d2c1d
KamyabT <dgghost034@gmail.com>
2021-05-01 17:14:08 +0430

    Question Box

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const btnIcon = document.querySelector('.open-btn i')
 openBtn.forEach(function(btn){
     btn.addEventListener('click', function(e){
         const clickedItem = e.currentTarget.parentElement.nextElementSibling
+        console.log(clickedItem);
         const boxBtnIcon = e.currentTarget.firstElementChild
         clickedItem.classList.toggle('openBox')
         if (clickedItem.classList.contains('openBox')) {
@@ -20,31 +21,3 @@ openBtn.forEach(function(btn){
     })
 })
 
-
-
-
-// boxCls.forEach(function(item){
-//     if (item.classList.contains('openBox')){
-//         item.classList.remove('openBox')
-//     } else
-// })
-
-
-
-
-
-// const boxBtnIcon = e.currentTarget.firstElementChild
-// boxCls.forEach(function(item){
-//     if (item.classList.contains('openBox')){
-//         item.classList.remove('openBox')
-//     } else {
-//         clickedItem.classList.toggle('openBox')
-//     }
-// })
-// if (clickedItem.classList.contains('openBox')){
-//     boxBtnIcon.classList.remove('fa-plus')
-//     boxBtnIcon.classList.add('fa-minus')
-// } else {
-//     boxBtnIcon.classList.add('fa-plus')
-//     boxBtnIcon.classList.remove('fa-minus') 
-// }
